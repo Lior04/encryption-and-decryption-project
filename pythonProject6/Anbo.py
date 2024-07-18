558d292b3722b26420d4ba8a3a1053c04c3adae4
LiorBr <86110425+Lior04@users.noreply.github.com>
2024-07-18 14:09:31 +0300

    Anbo.py

--- pythonProject6/Anbo.py
+++ pythonProject6/Anbo.py
@@ -1,10 +1,26 @@
 from Atbash import ABC
 abcHalf1 = ABC[:len(ABC) // 2]
-abcHalf2 = ABC[len(ABC) // 2]
+abcHalf2 = ABC[len(ABC) // 2:]
+
+
 
 class Anbo:
 
     def Encrypt(self, text):
+        encText = ""
+        for i in text:
+            if i != ' ':
+                indexLetter = abcHalf1.find(i)
+                if indexLetter == -1:
+                    indexLetter = abcHalf2.find(i)
+                    encText += abcHalf1[indexLetter]
+                else:
+                    encText += abcHalf2[indexLetter]
+            else:
+                encText += ' '
+        return encText
+
+    def Decrypt(self, text):
         decText = ""
         for i in text:
             if i != ' ':
@@ -14,4 +30,6 @@ class Anbo:
                     decText += abcHalf1[indexLetter]
                 else:
                     decText += abcHalf2[indexLetter]
+            else:
+                decText += ' '
         return decText
