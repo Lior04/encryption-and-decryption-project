8b7aa465315895ab8c9acc8194c17715f20a6316
LiorBr <86110425+Lior04@users.noreply.github.com>
2024-08-31 14:41:38 +0300

    Delete pythonProject6 directory

--- pythonProject6/Words.py
+++
@@ -1,8 +0,0 @@
-words = [
-    'spoon','sofa','pool','yellow','computer','chair','table','book',
-    'pencil','lamp','door','window','carpet','phone','camera','picture',
-    'clock','paper','pen','brush','bottle','plate','cup','glass','knife',
-    'fork','pillow','blanket','mirror','shelf','desk','rug','light','frame',
-    'wall','floor','celling','roof','stairs','elevator','garage','garden',
-    'yard','fence','gate','path','cheese','steak','dinosaur','horse','shark',
-    'tiger','airplane','encyclopedia','cube','one','three','phone','israel']
